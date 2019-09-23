6dd10be30a5f572240ca30ca4b19a7eaab48cc90
Noelle Lewandowski <tug48873@temple.edu>
2019-09-23 03:05:00 +0100

    fixed path

--- servo_control/test/sample.py
+++ servo_control/test/sample.py
@@ -4,7 +4,7 @@
 import RPi.GPIO as GPIO
 import time
 #This is the setup we are testing with the sample
-from servocntl_pkg.servo import servo
+from servocntl_pkg import servo
 
 #initialize the servo motor with a description, the pin number on the board (not the GPIO number
 #and the frequency at which the PWM signal should run (50Hz is average)
